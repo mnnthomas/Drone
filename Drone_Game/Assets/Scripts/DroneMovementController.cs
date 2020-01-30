e2241342a5cdfa98af08cd4bffc1c23615b86564
DeathQuake <mnn.thomas@gmail.com>
2020-01-30 17:17:35 +0530

    Adding DroneMovementController - Basic drone movement

---
+++ Drone_Game/Assets/Scripts/DroneMovementController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DroneGame
+{
+    public class DroneMovementController : MonoBehaviour
+    {
+        [SerializeField] private string m_HorizontalKey = default;
+        [SerializeField] private string m_ForwardKey = default;
+        [SerializeField] private string m_VerticalKey = default;
+        
+        [SerializeField] private float m_HorizontalSpeed = default;
+        [SerializeField] private float m_ForwardSpeed = default;
+        [SerializeField] private float m_VerticalSpeed = default;
+        [SerializeField] private float m_TurnAngle = default;
+        [SerializeField] private float m_TiltAngle = default;
+
+        private Rigidbody mRigidbody;
+
+        void Start()
+        {
+            mRigidbody = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            Vector3 movementVector = new Vector3(Input.GetAxis(m_HorizontalKey) * m_HorizontalSpeed, Input.GetAxis(m_VerticalKey) * m_VerticalSpeed, Input.GetAxis(m_ForwardKey) * m_ForwardSpeed) * Time.deltaTime;
+            Quaternion quaternion = Quaternion.Euler(Input.GetAxis(m_ForwardKey) * m_TiltAngle, 0f, -Input.GetAxis(m_HorizontalKey) * m_TiltAngle);
+
+            mRigidbody.MovePosition(transform.position + movementVector);
+            mRigidbody.MoveRotation(quaternion);
+        }
+    }
+}
+
