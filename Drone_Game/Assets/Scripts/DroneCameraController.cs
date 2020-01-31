c107ef6032c5fad11f6ed1369fc268e506dc963f
DeathQuake <mnn.thomas@gmail.com>
2020-01-31 17:04:12 +0530

    Drone movement and camera controls

---
+++ Drone_Game/Assets/Scripts/DroneCameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneCameraController : MonoBehaviour
+{
+    [SerializeField] private Transform m_DroneTransform;
+   // [SerializeField] private Transform m_TargetTransform;
+    [SerializeField] private string m_MouseXKey;
+    [SerializeField] private string m_MouseYKey;
+
+    [SerializeField] private float m_CameraTurnSpeed;
+    [SerializeField] private float m_DroneTurnSpeed;
+
+    private float mMouseX, mMouseY;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        mMouseX += Input.GetAxis(m_MouseXKey) * m_CameraTurnSpeed;
+        mMouseY -= Input.GetAxis(m_MouseYKey) * m_CameraTurnSpeed;
+        mMouseY = Mathf.Clamp(mMouseY, -35, 60);
+
+        UpdateCamera();
+        //UpdateDrone();
+    }
+
+
+    void UpdateCamera()
+    {
+        //transform.LookAt(m_TargetTransform);
+        Quaternion cameraTargetRot = Quaternion.Euler(mMouseY, mMouseX, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, cameraTargetRot, m_CameraTurnSpeed * Time.deltaTime);
+
+    }
+
+    void UpdateDrone()
+    {
+        Quaternion droneTargetRot = Quaternion.Euler(0, mMouseX, 0);
+        m_DroneTransform.rotation = Quaternion.Slerp(m_DroneTransform.rotation, droneTargetRot, m_DroneTurnSpeed * Time.deltaTime);
+    }
+}
--- Drone_Game/Assets/Scripts/DroneMovementController.cs
+++ Drone_Game/Assets/Scripts/DroneMovementController.cs
@@ -13,8 +13,10 @@ namespace DroneGame
         [SerializeField] private float m_HorizontalSpeed = default;
         [SerializeField] private float m_ForwardSpeed = default;
         [SerializeField] private float m_VerticalSpeed = default;
-        [SerializeField] private float m_TurnAngle = default;
+        [SerializeField] private float m_TurnSpeed = default;
         [SerializeField] private float m_TiltAngle = default;
+
+        [SerializeField] private Camera m_Camera = default;
 
         private Rigidbody mRigidbody;
 
@@ -25,11 +27,34 @@ namespace DroneGame
 
         void Update()
         {
+            Move();
+        }
+
+        private void LateUpdate()
+        {
+            Turn();
+
+        }
+
+        void Move()
+        {
             Vector3 movementVector = new Vector3(Input.GetAxis(m_HorizontalKey) * m_HorizontalSpeed, Input.GetAxis(m_VerticalKey) * m_VerticalSpeed, Input.GetAxis(m_ForwardKey) * m_ForwardSpeed) * Time.deltaTime;
             Quaternion quaternion = Quaternion.Euler(Input.GetAxis(m_ForwardKey) * m_TiltAngle, 0f, -Input.GetAxis(m_HorizontalKey) * m_TiltAngle);
 
-            mRigidbody.MovePosition(transform.position + movementVector);
-            mRigidbody.MoveRotation(quaternion);
+            transform.Translate(movementVector, Space.Self);
+            transform.GetChild(0).localRotation = quaternion;
+        }
+
+        void Turn()
+        {
+            Quaternion cameraRot = m_Camera.transform.rotation;
+            // Quaternion droneTargetRot = Quaternion.Euler(0, m_Camera.transform.localRotation.y, 0);
+            // Debug.Log(" >> " + m_Camera.transform.localRotation.y);
+            // transform.rotation = Quaternion.Slerp(transform.rotation, droneTargetRot, 5 * Time.deltaTime);
+
+            transform.forward = Vector3.Slerp(transform.forward, m_Camera.transform.forward, 5 * Time.deltaTime);
+            m_Camera.transform.rotation = cameraRot;
+
         }
     }
 }
