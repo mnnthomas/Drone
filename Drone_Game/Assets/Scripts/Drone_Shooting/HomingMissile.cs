bf29b1dc8d34d2d4d39c1b3301bfe41cbaa92912
DeathQuake <mnn.thomas@gmail.com>
2020-02-03 21:03:38 +0530

    added sounds for homing missile

--- Drone_Game/Assets/Scripts/Drone_Shooting/HomingMissile.cs
+++ Drone_Game/Assets/Scripts/Drone_Shooting/HomingMissile.cs
@@ -9,6 +9,10 @@ namespace DroneGame
     {
         [SerializeField] private float m_MissileLaunchForce = default;
         [SerializeField] private float m_MissileSeekStartDelay = default;
+        [SerializeField] private AudioSource m_AudioSource = default;
+        [SerializeField] private AudioClip m_MissileSeekClip = default;
+        [SerializeField] private AudioClip m_MissileFollowClip = default;
+
         private float mMissleLaunchTime;
         private bool mIsSeeking;
 
@@ -17,6 +21,12 @@ namespace DroneGame
             mCurTarget = target;
             mRigidbody.AddForce(m_MissileLaunchForce * forward, ForceMode.Force);
             transform.LookAt(Vector3.up);
+            if (m_AudioSource && m_MissileSeekClip)
+            {
+                m_AudioSource.clip = m_MissileSeekClip;
+                m_AudioSource.Play();
+            }
+
             mMissleLaunchTime = Time.time;
         }
 
@@ -25,6 +35,7 @@ namespace DroneGame
             if(Time.time - mMissleLaunchTime >= m_MissileSeekStartDelay && !mIsSeeking)
             {
                 mIsSeeking = true;
+                m_AudioSource.Stop();
             }
 
             SeekTarget();
@@ -33,8 +44,14 @@ namespace DroneGame
 
         private void SeekTarget()
         {
-            if(mIsSeeking && mCurTarget)
+            if (mIsSeeking && mCurTarget)
             {
+                if (m_AudioSource && m_MissileFollowClip && m_AudioSource.isPlaying != m_MissileFollowClip)
+                {
+                    m_AudioSource.clip = m_MissileFollowClip;
+                    m_AudioSource.Play();
+                }
+
                 mRigidbody.velocity = transform.forward * m_Speed;
                 Quaternion lookRot = Quaternion.LookRotation(mCurTarget.position - transform.position);
                 mRigidbody.MoveRotation(Quaternion.RotateTowards(transform.rotation, lookRot, m_TurnSpeed));
